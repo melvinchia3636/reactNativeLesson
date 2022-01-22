03d6e10783cf12255f2eab20aa0ab722f32706e2
melvinchia3636 <melvinchia623600@gmail.com>
2022-01-22 13:40:54 +0800

    UseState

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Pressable, StatusBar } from 'react-native';
+import { useState } from "react";
 
 export default function App() {
+
+  let [value, setValue] = useState(0)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{
+        backgroundColor: "white",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+        <Text style={{
+          color: "#22C55E",
+          fontSize: 64
+        }}>{value}</Text>
+        <Pressable style={{
+          backgroundColor: "#22C55E",
+          paddingHorizontal: 50,
+          paddingVertical: 20,
+          borderRadius: 6,
+          elevation: 4,
+          margin: 50
+        }}
+        onPress={() => {
+          setValue(value+1);
+          console.log(value)
+        }}>
+          <Text style={{
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 20
+          }}>Add to value</Text>
+        </Pressable>
+      </SafeAreaView>
+      <StatusBar backgroundColor="#22C55E" />
+    </>
   );
 }
 
