8104ba393d787108c7d91eb1307c9122c7e456bc
melvinchia3636 <melvinchia623600@gmail.com>
2022-01-22 13:48:12 +0800

    init

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { useState } from "react";
 
 export default function App() {
 
-  let [value, setValue] = useState(0)
-
   return (
     <>
       <SafeAreaView style={{
@@ -15,26 +13,8 @@ export default function App() {
       }}>
         <Text style={{
           color: "#22C55E",
-          fontSize: 64
-        }}>{value}</Text>
-        <Pressable style={{
-          backgroundColor: "#22C55E",
-          paddingHorizontal: 50,
-          paddingVertical: 20,
-          borderRadius: 6,
-          elevation: 4,
-          margin: 50
-        }}
-        onPress={() => {
-          setValue(value+1);
-          console.log(value)
-        }}>
-          <Text style={{
-            fontWeight: "bold",
-            color: "white",
-            fontSize: 20
-          }}>Add to value</Text>
-        </Pressable>
+          fontSize: 26
+        }}>Coming Soon</Text>
       </SafeAreaView>
       <StatusBar backgroundColor="#22C55E" />
     </>
